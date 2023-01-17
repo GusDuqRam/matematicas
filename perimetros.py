146cb2c6090aee013113331d77ebe9d6e5be0154
unknown <deporte920108@gmail.com>
2023-01-16 19:42:42 -0500

    Quinta modificacion

---
+++ perimetros.py
@@ -0,0 +1,17 @@
+def perCuadrado ():
+    lado= float(input("Ingrese el valor del perimetro: "))             
+    return print("El perimetro del cuadrado: ", lado*4) # 16 de Julio de 2023
+perCuadrado() # 18 de Julio de 2023
+
+def perRectangulo ():
+    base= float(input("Ingrese el valor de la base: "))   
+    altura= float(input("Ingrese el valor de la altura: "))          
+    return print("El perimetro del restanfulo: ", 2*base + 2*altura) # 16 de Julio de 2023
+perRectangulo() # 18 de Julio de 2023
+
+pi=3.1415
+def perCirculo ():
+    radio= float(input("Ingrese el valor del radio: "))
+    altura= float(input("Ingrese el valor de la altura: "))             
+    return print("El perimetro del circulo es: ", 2+pi*radio) # 16 de Julio de 2023
+perCirculo() # 18 de Julio de 2023
