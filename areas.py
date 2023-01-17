9294d697eb484f081c2694d1fc6c566532a7d432
unknown <deporte920108@gmail.com>
2023-01-16 19:23:33 -0500

    Cuarta modificacion

--- areas.py
+++ areas.py
@@ -8,3 +8,11 @@ def areaRectangulo ():
     altura= float(input("Ingrese el valor de la altura: "))             
     return print("El area del rectangulo: ", base*altura) # 16 de Julio de 2023
 areaRectangulo() # 18 de Julio de 2023
+
+pi=3.1415
+
+def areaCirculo ():
+    radio= float(input("Ingrese el valor del radio: "))
+    altura= float(input("Ingrese el valor de la altura: "))             
+    return print("El area del rectangulo: ", pi*altura**2) # 16 de Julio de 2023
+areaCirculo() # 18 de Julio de 2023
